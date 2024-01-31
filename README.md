4ec944712c507266d4bd73ad7f2db18899a67b99
XiangZi7 <793923048@qq.com>
2024-01-31 17:33:08 +0800

    初步搭建（4）

--- README.md
+++
@@ -1,1 +0,0 @@
-# yxcr-template
---
+++ yxcr-admin-pro/src/layouts/LayoutTransverse/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+// const state = reactive({
+//   list: [],
+// })
+// const {
+// list,
+// }= toRefs(state)
+</script>
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
